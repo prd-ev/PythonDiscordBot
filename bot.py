37c840fabc1a27405e3102cf21d4137be2ddeef3
BbqCoder <korba.adam@gmail.com>
2018-11-19 18:06:09 +0100

    Weather

--- bot.py
+++ bot.py
@@ -5,25 +5,39 @@ TOKEN = 'NTE0MDQ0MjQyOTYwODQyNzYy.DtQ5xA.2WR5-62vw-aPRq8IHL84ILT1Zpc'
 
 client = discord.Client()
 
-
-
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('!hello'):
-        msg = 'There'
+        msg = "There"
         await client.send_message(message.channel, msg)
 
-    elif message.content.startswith('!players'):
-
+    elif message.content.startswith('!mcplayers'):
         r = requests.get('https://mcapi.us/server/status?ip=pytatki-beta.pl')
         r_dict = json.loads(r.text)
         msg = "Na serwerze MC jest {0}/{1} graczy".format(r_dict["players"]["now"],r_dict["players"]["max"])
         await client.send_message(message.channel, msg)
-
+    
+    elif message.content.startswith('!weather'):
+        location = message.content[8:]
+        try:
+            api_key = "cad63d58be4335873686cf6efaf774bc"
+            url = "http://api.openweathermap.org/data/2.5/weather?q={0}&APPID={1}".format(location, api_key)
+            response = requests.get(url)
+            response.raise_for_status()
+            weather_data = json.loads(response.text)
+            msg = "Weather data for: {0}\nTemperature: {1} Celcius degrees\nHumidity: {2} %\nPressure: {3}hPa".format(
+                weather_data['name'],
+                int(weather_data['main']['temp']-272),
+                weather_data['main']['humidity'],
+                weather_data['main']['pressure']
+            )
+            await client.send_message(message.channel, msg)
+        except Exception:
+            msg = "Wrong syntax, try !weather [where]"
+            await client.send_message(message.channel, msg)
 
 @client.event
 async def on_ready():
